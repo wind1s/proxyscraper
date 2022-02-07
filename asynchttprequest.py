5deed18e2b3b8ee485169d1d45d3f581922059b5
William <william@local.com>
2022-02-07 21:30:14 +0200

    Updated asynchttp

--- asynchttprequest.py
+++ asynchttprequest.py
@@ -6,21 +6,29 @@ from itertools import islice
 from asyncio import ensure_future, sleep, run
 from aiohttp import ClientSession
 
-ProcessRequest = Callable[ClientSession, Any]
+ParseRequest = Callable[ClientSession, Any]
 
 
 class AsyncRequest:
-    """Wrapper class for storing request data and sending async request."""
+    """
+    Wrapper class for storing request data and sending async request.
+    """
 
     def __init__(self, method: str, url: str, **kwargs: Any):
         self.__data: dict[str, str] = {"method": method, "url": url, **kwargs}
 
+    @property
+    def method(self) -> str:
+        return self.__data["method"]
+
+    @property
+    def url(self) -> str:
+        return self.__data["url"]
+
     def get(self, key: str) -> str:
-        """"""
         return self.__data[key]
 
     def set(self, key: str, value: Any) -> None:
-        """"""
         self.__data[key] = value
 
     def __getitem__(self, key: str) -> str:
@@ -30,12 +38,12 @@ class AsyncRequest:
         self.set(key, value)
 
     async def send(self, session: ClientSession, **extra_data: Any) -> str:
-        """"""
         async with session.request(**self.__data, **extra_data) as response:
+            response.raise_for_status()
             return await response.read()
 
 
-async def limit_coros(coros: Iterable[Any], limit: int) -> Iterable[Any]:
+async def limited_as_completed(coros: Iterable[Any], limit: int) -> Iterable[Any]:
     """
     Runs a limited amount of coroutines at a time. Runs a new coroutine when one finishes.
     """
@@ -59,12 +67,14 @@ async def limit_coros(coros: Iterable[Any], limit: int) -> Iterable[Any]:
 
 
 def run_async_requests(
-    requests_data: Iterable[AsyncRequest],
-    process_request: Union[ProcessRequest, Iterable[ProcessRequest]],
+    requests_data: Iterable[Any],
+    process_request: Union[ParseRequest, Iterable[ParseRequest]],
     base_url: Optional[str] = None,
     limit: int = 1000,
 ) -> None:
-    """Creates coroutines for all requests and runs them async."""
+    """
+    Creates coroutines for all requests and runs them async.
+    """
 
     async def launch():
         async with ClientSession(base_url=base_url) as session:
@@ -73,6 +83,6 @@ def run_async_requests(
             else:
                 coros = (process_request(session, data) for data in requests_data)
 
-            return await limit_coros(coros, limit)
+            return await limited_as_completed(coros, limit)
 
     run(launch())
