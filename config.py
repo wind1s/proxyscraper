7d83229b5fbab855034e02918e226266cd099545
William <william@local.com>
2022-02-17 00:15:12 +0200

    Made cache path more robust with abspath()

--- config.py
+++ config.py
@@ -4,16 +4,17 @@
 * SPDX-License-Identifier: BSD-2-Clause
 """
 from datetime import timedelta
+from os.path import abspath
 
 # TODO: Move config data to a json file and init global variables from that file.
 # Rename this file to something like "configinit" or "readconfig".
 
 IP_DB_NAME = "ip.db"
-IP_DB_PATH = f"{IP_DB_NAME}"
-IP_DB_EXPIRE_TIME = timedelta(days=365)
+IP_DB_PATH = abspath(f"{IP_DB_NAME}")
+IP_DB_EXPIRE_TIME = timedelta(days=7)
 
 PROXY_DB_NAME = "proxies.db"
-PROXY_DB_PATH = f"{PROXY_DB_NAME}"
+PROXY_DB_PATH = abspath(f"{PROXY_DB_NAME}")
 PROXY_DB_EXPIRE_TIME = timedelta(hours=2)
 
 DEFAULT_OUTFILE = "iposint.json"
